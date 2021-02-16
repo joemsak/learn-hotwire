8b041736c3fb00c1411fd5e37e95fa9b10f636d1
Joe Sak <joe@joesak.com>
2021-02-15 19:15:09 -0600

    Set the default scope on tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,6 +3,8 @@ class Tweet < ApplicationRecord
 
   has_rich_text :content
 
+  default_scope -> { order(created_at: :desc) }
+
   after_create_commit { broadcast_prepend_to "tweets" }
   after_update_commit { broadcast_replace_to "tweets" }
   after_destroy_commit { broadcast_remove_to "tweets" }
