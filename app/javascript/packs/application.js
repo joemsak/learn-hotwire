cdb4601fcf242cc3d7abd5d7910ddb477e85b0a8
Joe Sak <joe@joesak.com>
2021-02-19 09:37:25 -0600

    Set the trix-editor cursor to the end of the content on initialize

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
@@ -16,3 +11,9 @@ require("@rails/actiontext")
 import "controllers"
 
 import "stylesheets/application"
+
+document.addEventListener('trix-initialize', e => {
+  const editor = e.target.editor
+  const endOfContent = editor.getDocument().getLength() - 1
+  editor.setSelectedRange(endOfContent)
+})
